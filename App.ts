b8f37fa420c0e94f6fc57ce4ae21ac77696dcfc0
Stephen (Alex) Wallen <wallenstephe@seattleu.edu>
2021-06-03 04:42:52 -1000

    Angular built dist works

--- App.ts
+++ App.ts
@@ -165,7 +165,7 @@ class App {
     this.expressApp.use("/", router);
     this.expressApp.use("/app/json/", express.static(__dirname + "/app/json"));
     this.expressApp.use("/images", express.static(__dirname + "/img"));
-    this.expressApp.use("/", express.static(__dirname + "/dist/index.html"));
+    this.expressApp.use("/", express.static(__dirname + "/dist"));
   }
 }
 
