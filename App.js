419f6ef5ef673e1fe65beaffd9f7a4808a4a108d
Stephen (Alex) Wallen <wallenstephe@seattleu.edu>
2021-06-03 20:20:50 -1000

    Updating dist folder name to angularSrc

--- App.js
+++ App.js
@@ -131,7 +131,7 @@ class App {
         this.expressApp.use("/", router);
         this.expressApp.use("/app/json/", express.static(__dirname + "/app/json"));
         this.expressApp.use("/images", express.static(__dirname + "/img"));
-        this.expressApp.use("/", express.static(__dirname + "/dist"));
+        this.expressApp.use("/", express.static(__dirname + "/angularSrc"));
     }
 }
 exports.App = App;
